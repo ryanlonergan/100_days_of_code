93fb7b2eb89ea6eabc4144ac965d425d963db23f
Ryan Lonergan <64630489+ryanlonergan@users.noreply.github.com>
2021-04-30 19:23:16 -0700

    Sorted out and fixed bearer token test
    spotipy exception would never trigger through oauth.py, so moved test to main to retain functionality

--- day_46_spotify_playlist_time_machine/main.py
+++ day_46_spotify_playlist_time_machine/main.py
@@ -1,10 +1,11 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-from oauth import get_bearer
-import json
-import spotipy
 import os
+import requests
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import spotipy
+
+from oauth import *
 
 # Can switch to an input to get custom dates, but I switch to a static date for testing
 # May want to add some error checking to ensure date is in correct format though
@@ -26,33 +27,19 @@ ranking = range(1, 101)  # I included a rank column as the index bothered me a b
 songs_df = pd.DataFrame(list(zip(ranking, song_list, artist_list)), columns=['Rank', 'Song', 'Artist'])
 # print(songs_df.to_string(index=False))  # prints the df without the index to avoid any confusion with rank
 
+# Get the bearer token from spotify using the functions within oauth.py
 bearer_token = get_bearer()
-bearer_check(bearer_token)
 
-sp = spotipy.Spotify(auth=bearer_token)
-user_details = sp.current_user()
-print(sp.current_user()['id'])
-
-
-
-
-
-# This section does a quick check to ensure that the recorded bearer token is still good
+# This section does a quick check to ensure that the recorded bearer token is still good and not expired
 # If it fails, it deletes the old token.txt file and requests it again
-# This action does require the user to copy and paste in their redirect url once they accept the app access from spotify
-def bearer_check(bearer_token):
-    """
-    Checks the recorded bearer token to make sure it works and is not expired. If an exception is raised, it removes
-    the token.txt file that acts as a cache and requests a new token
-    :param bearer_token: bearer token string to check
-    :return: verified bearer token string
-    """
-    try:
-        sp = spotipy.Spotify(auth=bearer_token)
-        user_details = sp.current_user()
-        print(user_details['id'])   # todo Figure out if this is needed later, do I need to call it again?
-    except spotipy.client.SpotifyException:
-        os.remove('token.txt')
-        bearer_token = get_oauth()
-    finally:
-        return bearer_token
+# This section used to be in a function within oauth.py, but spotipy did not like it there and the except statement
+# would never trigger. I am guessing it is just a peculiarity within the library
+try:
+    sp = spotipy.Spotify(auth=bearer_token)
+    user_details = sp.current_user()
+    print(user_details['id'])  # todo Figure out if this is needed later, do I need to call it again?
+except spotipy.client.SpotifyException:
+    os.remove('token.txt')
+    bearer_token = get_oauth()
+
+print(bearer_token)
