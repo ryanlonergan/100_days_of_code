5ca74e136ede5c98c966d033437c4c2ffbb7b14d
Ryan Lonergan <64630489+ryanlonergan@users.noreply.github.com>
2021-05-18 17:31:25 -0700

    Moved notes to notes.py

--- day_48_game_playing_bot/main.py
+++ day_48_game_playing_bot/main.py
@@ -1,12 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = r'C:\Users\rjl91\development\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-# testing Selenium - redo of day 47
-driver.get('https://www.amazon.com/Instant-Pot-Plus-Programmable-Sterilizer/dp/B08PQ2KWHS?ref_=ast_sto_dp&th=1')
-price = driver.find_element_by_id('priceblock_ourprice')
-print(price.text)
-
-# driver.close()  # close tab
-driver.quit()  # quit window
+driver.get('https://orteil.dashnet.org/cookieclicker/')
