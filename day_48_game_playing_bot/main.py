e5573dcfddc2d72c7d0d9f16ba359bcb276ac0e2
Ryan Lonergan <64630489+ryanlonergan@users.noreply.github.com>
2021-05-18 20:20:27 -0700

    Updated basic functionality - still needs purchase list

--- day_48_game_playing_bot/main.py
+++ day_48_game_playing_bot/main.py
@@ -5,3 +5,27 @@ chrome_driver_path = r'C:\Users\rjl91\development\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get('https://orteil.dashnet.org/cookieclicker/')
+
+big_cookie = driver.find_element_by_id('bigCookie')
+
+product_names = [f'product{i}' for i in range(17, -1, -1)]  # alternatively products = products[::-1]
+
+products = [driver.find_element_by_id('name') for name in product_names]
+
+for product in products:
+
+# while True:
+#     big_cookie.click()
+#
+#     product1 = driver.find_element_by_id('product1')
+#     if product1.get_attribute('class') == 'product unlocked enabled':
+#         product1.click()
+#     product = driver.find_element_by_id('product0')
+#     if product.get_attribute('class') == 'product unlocked enabled':
+#         product.click()
+#     try:
+#         upgrade = driver.find_element_by_id('upgrade0')
+#         if upgrade.get_attribute('class') == 'crate upgrade enabled':
+#             upgrade.click()
+#     except:
+#         continue
